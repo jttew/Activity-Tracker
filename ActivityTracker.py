43bdb0155af3639dc4b971418fce1bff82a34d03
jttew <jttew@crimson.ua.edu>
2017-07-01 04:51:18 -0400

    added new functions

--- ActivityTracker.py
+++ ActivityTracker.py
@@ -1,6 +1,22 @@
+import datetime
 import time
 
 def main():
     return 0
+
+def getDate():
+    now = datetime.datetime.now()
+    dateString = str(datetime.now())
+    return dateString
+
+def newActivity():
+    time.sleep(0.5) #We don't want to get two inputs on the same second
+    activityName = input("Current activity: ")
+    if not activityName.strip(): #if string is empty after removing whitespace
+        pass
+    time.sleep(0.5)
+    return activityName
+
+
 
 main()
